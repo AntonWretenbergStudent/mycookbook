b25c1654d4d4291ea5665cd25d4544f8fd3bf69f
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-04-07 15:22:43 +0200

    Started on the backend in the bookbook app

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+//import job from "./lib/cron.js"
+
+import authRoutes from "./routes/authRoutes.js"
+import recipesRoutes from "./routes/recipesRoutes.js"
+
+import { connectDB } from "./lib/db.js"
+
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+//job.start()
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/recipes", recipesRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB()
+})
