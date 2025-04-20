e54a1382a14c46df388b4bbf1a187c38e86e10be
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2025-04-20 21:27:39 +0200

    Add image size limit error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,22 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
-//import job from "./lib/cron.js"
-
 import authRoutes from "./routes/authRoutes.js"
 import recipesRoutes from "./routes/recipesRoutes.js"
 
 import { connectDB } from "./lib/db.js"
 
-
 const app = express()
 const PORT = process.env.PORT || 4000
 
-//job.start()
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
+
 app.use(cors())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/recipes", recipesRoutes)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
